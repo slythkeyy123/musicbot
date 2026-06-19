023564b16446260be8c883354ad3a56269c0c0d5
slythkeyy123 <mhammadrzky364@gmail.com>
2026-06-19 13:11:06 +0700

    Refactor Discord bot commands for clarity

--- index.js
+++ index.js
@@ -1,110 +1,101 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const {
-    joinVoiceChannel,
-    createAudioPlayer,
-    createAudioResource,
-    AudioPlayerStatus,
-    getVoiceConnection
+  joinVoiceChannel,
+  createAudioPlayer,
+  createAudioResource,
+  getVoiceConnection
 } = require('@discordjs/voice');
 const play = require('play-dl');
 
 const TOKEN = process.env.TOKEN;
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildVoiceStates
-    ]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates
+  ]
 });
 
 const player = createAudioPlayer();
 
 client.once('ready', () => {
-    console.log(`${client.user.tag} Online!`);
+  console.log(`${client.user.tag} Online!`);
 });
 
-client.on('messageCreate', async (msg) => {
+client.on('messageCreate', async msg => {
 
-    if (msg.author.bot) return;
+  if (msg.author.bot) return;
 
-    // !ping
-    if (msg.content === '!ping') {
-        return msg.reply('Pong!');
+  if (msg.content === '!ping') {
+    return msg.reply('Pong!');
+  }
+
+  if (msg.content === '!join') {
+
+    if (!msg.member.voice.channel)
+      return msg.reply('Masuk voice channel dulu!');
+
+    joinVoiceChannel({
+      channelId: msg.member.voice.channel.id,
+      guildId: msg.guild.id,
+      adapterCreator: msg.guild.voiceAdapterCreator
+    });
+
+    return msg.reply('Masuk voice channel!');
+  }
+
+  if (msg.content === '!leave') {
+
+    const connection = getVoiceConnection(msg.guild.id);
+
+    if (!connection)
+      return msg.reply('Bot tidak ada di voice.');
+
+    connection.destroy();
+
+    return msg.reply('Keluar voice channel!');
+  }
+
+  if (msg.content === '!stop') {
+    player.stop();
+    return msg.reply('Musik dihentikan.');
+  }
+
+  if (msg.content.startsWith('!play ')) {
+
+    if (!msg.member.voice.channel)
+      return msg.reply('Masuk voice channel dulu!');
+
+    const url = msg.content.split(' ')[1];
+
+    try {
+
+      const connection = joinVoiceChannel({
+        channelId: msg.member.voice.channel.id,
+        guildId: msg.guild.id,
+        adapterCreator: msg.guild.voiceAdapterCreator
+      });
+
+      const stream = await play.stream(url);
+
+      const resource = createAudioResource(stream.stream, {
+        inputType: stream.type
+      });
+
+      player.play(resource);
+      connection.subscribe(player);
+
+      msg.reply('Memutar musik...');
+
+    } catch (err) {
+
+      console.log(err);
+      msg.reply('Gagal memutar lagu.');
+
     }
-
-    // !join
-    if (msg.content === '!join') {
-
-        if (!msg.member.voice.channel)
-            return msg.reply('Masuk voice channel dulu!');
-
-        joinVoiceChannel({
-            channelId: msg.member.voice.channel.id,
-            guildId: msg.guild.id,
-            adapterCreator: msg.guild.voiceAdapterCreator
-        });
-
-        return msg.reply('Masuk voice channel!');
-    }
-
-    // !leave
-    if (msg.content === '!leave') {
-
-        const connection = getVoiceConnection(msg.guild.id);
-
-        if (!connection)
-            return msg.reply('Bot tidak ada di voice.');
-
-        connection.destroy();
-
-        return msg.reply('Keluar voice channel!');
-    }
-
-    // !stop
-    if (msg.content === '!stop') {
-
-        player.stop();
-
-        return msg.reply('Musik dihentikan.');
-    }
-
-    // !play LINK
-    if (msg.content.startsWith('!play ')) {
-
-        if (!msg.member.voice.channel)
-            return msg.reply('Masuk voice channel dulu!');
-
-        const url = msg.content.split(' ')[1];
-
-        const connection = joinVoiceChannel({
-            channelId: msg.member.voice.channel.id,
-            guildId: msg.guild.id,
-            adapterCreator: msg.guild.voiceAdapterCreator
-        });
-
-        try {
-
-            const stream = await play.stream(url);
-
-            const resource = createAudioResource(stream.stream, {
-                inputType: stream.type
-            });
-
-            player.play(resource);
-
-            connection.subscribe(player);
-
-            msg.reply('Memutar musik...');
-
-        } catch (err) {
-
-            console.log(err);
-
-            msg.reply('Gagal memutar lagu.');
-        }
-    }
+  }
 
 });
 
